d3d98f966dabdd2e208d03a1643026e66625edac
Stephen Thouvenot <39969373+sthouvenot@users.noreply.github.com>
2020-07-29 18:04:54 -0500

    Update bootstrap-fauna-database.js

--- scripts/bootstrap-fauna-database.js
+++ scripts/bootstrap-fauna-database.js
@@ -6,19 +6,9 @@ const q = faunadb.query
 
 console.log(chalk.cyan('Creating your FaunaDB Database...\n'))
 
-// 1. Check for required enviroment variables
-if (!process.env.FAUNADB_SERVER_SECRET) {
-  console.log(chalk.yellow('Required FAUNADB_SERVER_SECRET enviroment variable not found.'))
-  console.log(`Make sure you have created your Fauna databse with "netlify addons:create fauna"`)
-  console.log(`Then run "npm run bootstrap" to setup your database schema`)
-  if (insideNetlify) {
-    process.exit(1)
-  }
-}
-
 // Has var. Do the thing
 if (process.env.FAUNADB_SERVER_SECRET) {
-  createFaunaDB(process.env.FAUNADB_SERVER_SECRET).then(() => {
+  createFaunaDB(fnADx9Xi3PACACiTIjc2Pb7z4cNM2Ij6etRQYktT).then(() => {
     console.log('Fauna Database schema has been created')
     console.log('Claim your fauna database with "netlify addons:auth fauna"')
   })
