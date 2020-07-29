13d990c992b252481a069a3558d76e63f384e044
Stephen Thouvenot <39969373+sthouvenot@users.noreply.github.com>
2020-07-29 18:06:06 -0500

    Update todos-read-all.js

--- functions/todos-read-all.js
+++ functions/todos-read-all.js
@@ -7,7 +7,7 @@ exports.handler = (event, context) => {
   console.log('Function `todo-read-all` invoked')
   /* configure faunaDB Client with our secret */
   const client = new faunadb.Client({
-    secret: process.env.FAUNADB_SERVER_SECRET
+    secret: fnADx9Xi3PACACiTIjc2Pb7z4cNM2Ij6etRQYktT
   }) 
   return client.query(q.Paginate(q.Match(q.Ref('indexes/all_todos'))))
     .then((response) => {
